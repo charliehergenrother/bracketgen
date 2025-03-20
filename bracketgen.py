d2dbd1f1ba2b8a6ded671ceee9432cfc47fd0a0f
Charlie Hergenrother <charliehergenrother@yahoo.com>
2025-03-20 14:28:46 -0500

    wisconsin

--- bracketgen.py
+++ bracketgen.py
@@ -40,7 +40,7 @@ def fill_winners(mens):
         #MATCHUP_WINNERS[18] = "Miami (FL)"
         #MATCHUP_WINNERS[19] = "Indiana"
         #MATCHUP_WINNERS[20] = "Iowa State"
-        #MATCHUP_WINNERS[21] = "Xavier"
+        MATCHUP_WINNERS[21] = "Wisconsin"
         #MATCHUP_WINNERS[22] = "Texas A&M"
         #MATCHUP_WINNERS[23] = "Texas"
         #MATCHUP_WINNERS[24] = "Kansas"
