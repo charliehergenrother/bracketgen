4033d9086f4aa7ae691723f84b56019079d24417
Charlie Hergenrother <charliehergenrother@yahoo.com>
2025-03-21 12:44:15 -0500

    michigan w

--- bracketgen.py
+++ bracketgen.py
@@ -119,7 +119,7 @@ def fill_winners(mens):
         #MATCHUP_WINNERS[25] = "USC"
         #MATCHUP_WINNERS[26] = "Iowa State"
         #MATCHUP_WINNERS[27] = "Tennessee"
-        #MATCHUP_WINNERS[28] = "North Carolina"
+        MATCHUP_WINNERS[28] = "Michigan"
         #MATCHUP_WINNERS[29] = "Ohio State"
         #MATCHUP_WINNERS[30] = "Baylor"
         #MATCHUP_WINNERS[31] = "Connecticut"
